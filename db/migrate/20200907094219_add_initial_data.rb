b0a50800f478501b30ea4f4bec4914c999b16c3b
Fabricio Obregon <fabricioobregon@live.com>
2020-09-07 09:49:38 -0600

    update my app

--- db/migrate/20200907094219_add_initial_data.rb
+++ db/migrate/20200907094219_add_initial_data.rb
@@ -5,7 +5,7 @@ class AddInitialData < ActiveRecord::Migration[6.0]
     firstCard = Card.create(name: 'My first card', golden: 'true', cardType: 'Minion', race: 'Druid',
                             classone: 'no idea', classtwo: 'no idea 2', rarity: 'unique', attack: '20', cost: '10',
                             health: '50', set: 'Strongest Set', description: 'The first set')
-    firstDeck = Deck.create(name: 'My first deck', user_id: 1)
+    firstDeck = Deck.create(name: 'My first deck', user_id: firstUser.id)
 
     firstUser.cards << firstCard
 
@@ -16,10 +16,8 @@ class AddInitialData < ActiveRecord::Migration[6.0]
 
   def down
 
-    firstUser = User.find(2)
-    firstUser.cards.clear
-    firstDeck = Deck.find(1)
-    firstDeck.cards.clear
+    User.first.cards.clear
+    Deck.first.cards.clear
     Deck.delete_all
     User.delete_all
     Card.delete_all
